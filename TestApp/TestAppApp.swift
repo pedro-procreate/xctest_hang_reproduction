ad93ae817d8d684b683ad93d74237a358831b7ec
pedro-procreate <pedro@procreate.com>
2026-03-13 09:18:32 +1100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Pedro Tacla Yamada on 13/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppTests.swift
+//  TestAppTests
+//
+//  Created by Pedro Tacla Yamada on 13/3/2026.
+//
+
+import Testing
+@testable import TestApp
+
+struct TestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
